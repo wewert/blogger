479e413f0642b07573626af9674d083718cfa574
Ken Lee <wewert@gmail.com>
2017-03-27 01:03:02 -0600

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Blogger::Application.routes.draw do
-  root to: 'articles#index'
-  resources :articles do
-    resources :comments
+    root to: 'articles#index'
+    resources :articles do
+      resources :comments
+    end
+    resources :tags
+
   end
-end
 # Blogger::Application.routes.draw do
 
 
